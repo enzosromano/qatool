54cddd922be6b6050befb2bcc9cc79a19d4efdea
enzosromano <enzosromano@gmail.com>
2024-05-08 17:09:53 -0400

    example script

--- src/scripts/test_ex.py
+++ src/scripts/test_ex.py
@@ -8,3 +8,5 @@ class TestHome():
         options.add_argument('--ignore-ssl-errors')
         options.add_argument("window-size=1200x600")
         self.driver = webdriver.Chrome(options=options)
+
+    def test_example(): #Pytest will only run your functions if their names begin with "test"
