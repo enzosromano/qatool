bcbd98edf1353fa98bc760a4f3b83d39346755dd
enzosromano <enzosromano@gmail.com>
2024-05-08 17:38:38 -0400

    removed init

--- src/scripts/test_ex.py
+++ src/scripts/test_ex.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 
-class TestHome():
-    def __init__(self):
-        # Create driver
+class TestExample():
+
+    def test_example(self): #Pytest will only run your functions if their names begin with "test"
         options = webdriver.ChromeOptions()
         options.add_argument('ignore-certificate-errors')
         options.add_argument('--ignore-ssl-errors')
         options.add_argument("window-size=1200x600")
         self.driver = webdriver.Chrome(options=options)
+        self.driver.get("https://enzosromano.github.io/portfolio/")
+        assert "Enzo Romano" in self.driver.title
 
-    def test_example(): #Pytest will only run your functions if their names begin with "test"
+        self.driver.close()
+        self.driver.quit()
